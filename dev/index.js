7dd9beca8eccac8802ad3720f8b64202fe22de22
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2018-01-08 14:07:52 +0800

    init

---
+++ dev/index.js
@@ -0,0 +1,4 @@
+import './js/libs/symbol'
+import Main from './js/main'
+
+new Main()
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  // 入口文件名称
+  entry: './dev/index.js',
+  // 输出文件名称
+  output: {
+      // 文件目录
+      path: path.resolve(__dirname, 'dep'),
+      // 文件名字
+      filename: 'bundle.js'
+  },
+  // 开启热更新
+  devServer: {
+    // 设置端口的根目录
+    contentBase: path.join(__dirname, "dep"),
+    // 是否开启压缩代码
+    compress: true,
+    // 监听的端口
+    port: 4000
+  }
+}
